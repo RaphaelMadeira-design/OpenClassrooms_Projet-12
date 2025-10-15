ab1e5db6cdd27ec2d8108d77f7000f84feae5425
Raphaël Madeira <raphael.m21@gmail.com>
2025-10-15 15:38:26 +0200

    Firefox

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -10,11 +10,19 @@ function Loading() {
                 <div className="loader-goo--ball"></div>
                 <div className="loader-goo--ball"></div>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
                 <defs>
-                    <filter id="goo">
+                    <filter id="goo" filterUnits="userSpaceOnUse">
                         <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                        <feColorMatrix
+                            in="blur"
+                            mode="matrix"
+                            values="1 0 0 0 0  
+                                    0 1 0 0 0  
+                                    0 0 1 0 0  
+                                    0 0 0 18 -7"
+                            result="goo"
+                        />
                     </filter>
                 </defs>
             </svg>
